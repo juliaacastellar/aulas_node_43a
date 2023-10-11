f05d5579aa16c1595104891964d183c29b1c49dd
Julia Castellar <castellar2003j@gmail.com>
2023-10-10 22:25:08 -0300

    with

---
+++ 3_HANDLEBARS/6_with/index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+// configurando o handlebars como template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get ('/post', (req, res) => {
+    const post = {
+        title: "Aprendendo Node.js",
+        category: "Javascript",
+        body: "artigo em construção",
+        comments: 4
+    }
+
+    res.render('post', {post})
+} )
+
+app.get('/dashboard', (req, res) =>  {
+    const items = ['item a', 'item b', 'item c']
+
+    res.render('dashboard', {items})
+})
+
+app.get('/', (req, res) => {
+    const usuario = {
+        nome: "Julia",
+        profissao: "Aluno",
+        idade: 20
+    }
+    const auth = true
+
+
+    res.render('home', {usuario, auth})
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
